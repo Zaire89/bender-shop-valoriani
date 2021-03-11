5613444e887d539dd4c471fda035e1e539bdc759
Zaire Valoriani <zvfoto@hotmail.com>
2021-03-11 00:49:24 -0300

    barra nav + estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo01.png';
 import './App.css';
+import NavBar from './components/Nav-bar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tienda Bender
-        </p>
-        <a
-          className="App-link"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go!
-        </a>
-      </header>
+
+      <NavBar />
+
+
     </div>
   );
 }
---
+++ src/components/Nav-bar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo01 from './logo01.png';
+import './Nav-bar.css';
+
+
+export const NavBar =() => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-dark  d-flex flex-row" className="navBarrr">
+
+            <a class="navbar-brand" href="#" className="logo" class="ml-auto"> <img src={logo01} alt="logo" height="80px" /></a>
+            
+            <div class="collapse navbar-collapse d-flex flex-row justify-content-end" id="navbarNav" >
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"> Inicio </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#listado" id="navbarDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Productos </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown"  >
+                            <a class="dropdown-item" href="#"> Remeras </a>
+                            <a class="dropdown-item" href="#"> Tazas </a>
+                            <a class="dropdown-item" href="#"> Túnicas virtuales </a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Contacto </a>
+                    </li>
+
+                </ul>
+
+                <button class="btn my-2 my-sm-0 my-lg-0" type="submit" id="descarga" >Descargar Juego</button>
+            </div>
+
+
+        </nav>
+
+
+
+
+    );
+};
+
+export default NavBar;
